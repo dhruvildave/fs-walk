ca504d38919dd952b9f0b863ae1d750b94d1a9af
Dhruvil Dave <43331416+dhruvildave@users.noreply.github.com>
2023-11-03 23:46:44 -0500

    Dependency bump

--- src/main.ts
+++ src/main.ts
@@ -63,7 +63,7 @@ function include(
   path: string,
   exts?: string[],
   match?: RegExp[],
-  skip?: RegExp[]
+  skip?: RegExp[],
 ): boolean {
   if (exts && !exts.some((ext): boolean => path.endsWith(ext))) {
     return false;
@@ -100,7 +100,7 @@ export async function* walk(
     exts = undefined,
     match = undefined,
     skip = undefined,
-  }: WalkOptions = {}
+  }: WalkOptions = {},
 ): AsyncIterableIterator<WalkEntry> {
   if (maxDepth < 0) {
     return;
@@ -177,7 +177,7 @@ export function* walkSync(
     exts = undefined,
     match = undefined,
     skip = undefined,
-  }: WalkOptions = {}
+  }: WalkOptions = {},
 ): IterableIterator<WalkEntry> {
   if (maxDepth < 0) {
     return;
